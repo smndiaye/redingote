c444c422cf9da956b6d568564969476a43b99c72
smndiaye <serigne.ndiaye@monstar-lab.com>
2017-10-11 18:55:39 +0900

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def hello
-    setupFile = "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+    setupFile = "ddddddddddsfdsfsdfsfddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
   end
 end
