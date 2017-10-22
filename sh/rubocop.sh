be81acbea120cf267d371ed7fcfc122ee7b28213
smndiaye <serigne.ndiaye@monstar-lab.com>
2017-10-22 20:09:45 +0900

    tt

--- sh/rubocop.sh
+++ sh/rubocop.sh
@@ -22,7 +22,7 @@ echo "pull request number is $PR_NUMBER"
 
 echo 'add comment to github'
 
-COV_URL="https://$PR_NUMBER-105956307-gh.circle-artifacts.com/0/coverage/index.html"
+COV_URL="https://$CIRCLE_BUILD_NUM-105956307-gh.circle-artifacts.com/0/coverage/index.html"
 COMMENT_BODY="Coverage report\\n$COV_URL\\n"
 POST_BODY="{\"body\": \"$COMMENT_BODY\"}"
 curl -XPOST \
