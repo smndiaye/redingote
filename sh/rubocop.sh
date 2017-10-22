83c608f5358403223d304202c7bc9e9a5c5c1f86
smndiaye <serigne.ndiaye@monstar-lab.com>
2017-10-22 21:07:27 +0900

    tt

--- sh/rubocop.sh
+++ sh/rubocop.sh
@@ -2,7 +2,7 @@
 
 echo 'check if pull request'
 
-if [ "$CI_PULL_REQUEST" == false ] || [ -z "$CI_PULL_REQUEST+x" ]; then
+if [ "$CI_PULL_REQUEST" == false ] || [ -z "${CI_PULL_REQUEST:-}" ]; then
   echo 'not a pull request';
   exit 0;
 fi
@@ -16,14 +16,10 @@ else
   exit 0;
 fi
 
-echo 'add comment to github PR'
+echo 'post code coverage as github PR comment'
 
-COV_URL="https://$CIRCLE_BUILD_NUM-105956307-gh.circle-artifacts.com/0/coverage/index.html"
 PERCENTAGE=`cat coverage/.last_run.json | jq '.result.covered_percent'`
-COMMENT_BODY="Code coverage is $PERCENTAGE%\\nFor more details\\n$COV_URL"
+COMMENT_BODY="Code coverage is $PERCENTAGE%\\nFor more details\\n [Code coverage is $PERCENTAGE%](https://$CIRCLE_BUILD_NUM-105956307-gh.circle-artifacts.com/0/coverage/index.html)"
 POST_BODY="{\"body\": \"$COMMENT_BODY\"}"
-curl -XPOST \
-  -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
-  -H "Content-Type: application/json" \
-  -d "$POST_BODY" \
-  https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues/$PR_NUMBER/comments
+curl -XPOST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Content-Type: application/json" \
+  -d "$POST_BODY" https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues/$PR_NUMBER/comments
