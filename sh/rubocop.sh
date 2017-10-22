ba6a0d3ece0573bcb386921e30d17f8f8035c0bc
smndiaye <serigne.ndiaye@monstar-lab.com>
2017-10-22 21:14:07 +0900

    tt

--- sh/rubocop.sh
+++ sh/rubocop.sh
@@ -19,7 +19,8 @@ fi
 echo 'post code coverage as github PR comment'
 
 PERCENTAGE=`cat coverage/.last_run.json | jq '.result.covered_percent'`
-COMMENT_BODY="Code coverage is $PERCENTAGE%\\nFor more details\\n [Code coverage is $PERCENTAGE%](https://$CIRCLE_BUILD_NUM-105956307-gh.circle-artifacts.com/0/coverage/index.html)"
-POST_BODY="{\"body\": \"$COMMENT_BODY\"}"
+SIMPLE_COV_URL="https://$CIRCLE_BUILD_NUM-105956307-gh.circle-artifacts.com/0/coverage/index.html"
+COMMENT_BODY="[Code coverage is $PERCENTAGE%]($SIMPLE_COV_URL)"
 curl -XPOST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Content-Type: application/json" \
-  -d "$POST_BODY" https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues/$PR_NUMBER/comments
+  -d "{\"body\": \"$COMMENT_BODY\"}" \
+  https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues/$PR_NUMBER/comments
