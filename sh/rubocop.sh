4d593c6fd4e2eaa2a37710372537487196ed64f8
smndiaye <serigne.ndiaye@monstar-lab.com>
2018-02-02 18:12:35 +0900

    rrrr

--- sh/rubocop.sh
+++ sh/rubocop.sh
@@ -1,21 +1,19 @@
 set -eu
 echo 'check if pull request'
 
-if [ -z ${CI_PULL_REQUEST+x} ] || [ "$CI_PULL_REQUEST" == false ] ; then
-  echo 'not a pull request';
-  exit 0;
-fi
-
 PERCENTAGE=`cat coverage/.last_run.json | jq '.result.covered_percent'`
 SIMPLE_COV_URL="https://$CIRCLE_BUILD_NUM-91943388-gh.circle-artifacts.com/0/coverage/index.html"
 COMMENT_BODY="[Code coverage is now at $PERCENTAGE%]($SIMPLE_COV_URL)"
 
-if [[ $CI_PULL_REQUEST =~ ([0-9]*)$ ]]; then
-  echo 'get pull request number'
-  PR_NUMBER=${BASH_REMATCH[1]}
-  curl -XPOST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Content-Type: application/json" \
-  -d "{\"body\": \"$COMMENT_BODY\"}" \
-  https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues/$PR_NUMBER/comments
+
+if [ -z ${CI_PULL_REQUEST+x} ] || [ "$CI_PULL_REQUEST" == false ] ; then
+  if [[ $CI_PULL_REQUEST =~ ([0-9]*)$ ]]; then
+      echo 'get pull request number'
+      PR_NUMBER=${BASH_REMATCH[1]}
+      curl -XPOST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Content-Type: application/json" \
+      -d "{\"body\": \"$COMMENT_BODY\"}" \
+      https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues/$PR_NUMBER/comments
+  fi
 else
   echo 'get commit sha';
   curl -XPOST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Content-Type: application/json" \
