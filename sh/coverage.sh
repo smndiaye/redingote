3cdf8b054d471495971c34c6b13cf0b431aa599c
smndiaye <serigne.ndiaye@monstar-lab.com>
2018-02-02 18:45:37 +0900

    rrrr

---
+++ sh/coverage.sh
@@ -0,0 +1,21 @@
+set -eu
+
+PERCENTAGE=`cat coverage/.last_run.json | jq '.result.covered_percent'`
+SIMPLE_COV_URL="https://$CIRCLE_BUILD_NUM-91943388-gh.circle-artifacts.com/0/coverage/index.html"
+COMMENT_BODY="[Code coverage is now at $PERCENTAGE%]($SIMPLE_COV_URL)"
+
+
+if [ -z ${CI_PULL_REQUEST+x} ] || [ "$CI_PULL_REQUEST" == false ] ; then
+  echo 'get commit sha';
+  curl -XPOST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Content-Type: application/json" \
+  -d "{\"body\": \"$COMMENT_BODY\"}" \
+  https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments
+else
+  if [[ $CI_PULL_REQUEST =~ ([0-9]*)$ ]]; then
+      echo 'get pull request number'
+      PR_NUMBER=${BASH_REMATCH[1]}
+      curl -XPOST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Content-Type: application/json" \
+      -d "{\"body\": \"$COMMENT_BODY\"}" \
+      https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues/$PR_NUMBER/comments
+  fi
+fi
--- sh/rubocop.sh
+++ sh/rubocop.sh
@@ -1,22 +1,65 @@
-set -eu
-echo 'check if pull request'
+#!/usr/bin/env bash
 
-PERCENTAGE=`cat coverage/.last_run.json | jq '.result.covered_percent'`
-SIMPLE_COV_URL="https://$CIRCLE_BUILD_NUM-91943388-gh.circle-artifacts.com/0/coverage/index.html"
-COMMENT_BODY="[Code coverage is now at $PERCENTAGE%]($SIMPLE_COV_URL)"
-
-
-if [ -z ${CI_PULL_REQUEST+x} ] || [ "$CI_PULL_REQUEST" == false ] ; then
-  echo 'get commit sha';
-  curl -XPOST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Content-Type: application/json" \
-  -d "{\"body\": \"$COMMENT_BODY\"}" \
-  https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments
-else
-  if [[ $CI_PULL_REQUEST =~ ([0-9]*)$ ]]; then
-      echo 'get pull request number'
-      PR_NUMBER=${BASH_REMATCH[1]}
-      curl -XPOST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Content-Type: application/json" \
-      -d "{\"body\": \"$COMMENT_BODY\"}" \
-      https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues/$PR_NUMBER/comments
-  fi
+# Skip conditions
+if [ "${CIRCLE_BRANCH}" == 'master' ]; then
+  exit 0
 fi
+
+# Install gems
+gem i --quiet --no-document \
+  checkstyle_filter-git \
+  saddler \
+  saddler-reporter-github \
+  rubocop \
+  rubocop-select \
+  rubocop-checkstyle_formatter \
+  rails_best_practices \
+  reek
+
+# Set reporter
+if [ -z "${CI_PULL_REQUEST}" ]; then
+  REPORTER=Saddler::Reporter::Github::CommitReviewComment
+else
+  REPORTER=Saddler::Reporter::Github::PullRequestReviewComment
+fi
+
+# Get diffed files
+RUBY_DIFFS=$(git diff -z --name-only origin/master | xargs -0 rubocop-select)
+
+echo "******************************"
+echo "*          RuboCop           *"
+echo "******************************"
+
+echo "$RUBY_DIFFS" | xargs rubocop --require "$(gem which rubocop/formatter/checkstyle_formatter)" \
+                                   --format RuboCop::Formatter::CheckstyleFormatter \
+                                   --out rubocop.xml
+
+cat rubocop.xml | \
+    checkstyle_filter-git diff origin/master | \
+    saddler report --require saddler/reporter/github --reporter $REPORTER
+
+cp -v 'rubocop.xml' "$CIRCLE_ARTIFACTS/"
+
+echo "******************************"
+echo "*     Rails Best Pratices    *"
+echo "******************************"
+
+echo "$RUBY_DIFFS" | xargs rails_best_practices --format xml
+
+cat rails_best_practices_output.xml | \
+    checkstyle_filter-git diff origin/master | \
+    saddler report --require saddler/reporter/github --reporter $REPORTER
+
+cp -v 'rails_best_practices_output.xml' "$CIRCLE_ARTIFACTS/"
+
+echo "******************************"
+echo "*            Reek            *"
+echo "******************************"
+
+echo "$RUBY_DIFFS" | xargs reek app --format xml > reek.xml
+
+cat reek.xml | \
+    checkstyle_filter-git diff origin/master | \
+    saddler report --require saddler/reporter/github --reporter $REPORTER
+
+cp -v 'reek.xml' "$CIRCLE_ARTIFACTS/"
