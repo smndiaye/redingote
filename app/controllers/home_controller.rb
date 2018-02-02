7d2770323452989f8f6e3783676a48b2e1c3dd58
smndiaye <serigne.ndiaye@monstar-lab.com>
2018-02-02 18:59:44 +0900

    rrrr

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def hello
     setupFile = 'ddddddddddsfdsfsdfsfdddddddddddddjnjknjkjkmmmmdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd'
     l = 0
+    valerie = -1
   end
 end
